00d762d9d436e39b6b9b20fd3305f1f3747953d5
travelistars <travelistars@gmail.com>
2016-01-11 16:51:00 +0000

    almost done, but need to fix burthday

--- microposts/app/controllers/users_controller.rb
+++ microposts/app/controllers/users_controller.rb
@@ -17,12 +17,27 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
-end
+  end
+    
+    def edit
+      @user = User.find(params[:id])
+    end
+    
+    def update
+      @user = User.find(params[:id])
+
+      if @user.update(user_params)
+        redirect_to current_user , notice: 'ユーザー情報を編集しました'
+      else
+        render 'edit'
+      end
+    end
+    
 
 private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :nickname, :gender, :birthday, :description)
   end
   
 end
--- microposts/app/models/user.rb
+++ microposts/app/models/user.rb
@@ -5,5 +5,6 @@ class User < ActiveRecord::Base
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: { case_sensitive: false }
+    validates :nickname, presence: false, length: {maximum: 50 }
     has_secure_password
 end
