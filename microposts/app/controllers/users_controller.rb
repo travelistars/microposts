9c2d78bc16fd1711cc62213ae38146f56f1cfe39
travelistars <travelistars@gmail.com>
2016-01-12 06:07:03 +0000

    seg

--- microposts/app/controllers/users_controller.rb
+++ microposts/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 I18n.locale = "ja"
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following_users, :follower_users]
   
   def show
     @user = User.find(params[:id])
@@ -18,6 +20,21 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+    
+    def following_users
+      @title = "Following"
+      @user = User.find(params[:id])
+      @users = @user.following_users.paginate(page: params[:page])
+      render 'show_follow'
+    end
+    
+    def follower_users
+      @title = "Followers"
+      @user  = User.find(params[:id])
+      @users = @user.follower_users.paginate(page: params[:page])
+      render 'show_follow'
+    end
+    
 end
 
 private
@@ -25,5 +42,7 @@ private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  
+    
   
 end
