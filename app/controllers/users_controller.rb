8951a5ae599ee529e04b9ad646ae8f07e8b67171
travelistars <travelistars@gmail.com>
2016-01-12 08:41:34 +0000

    tempo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 I18n.locale = "ja"
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following_users, :follower_users]
+  before_action :correct_user, only:[:edit, :update]
+  def index
+    @users = User.all
+  end
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(created_at: :desc)
   end
   
   def new
@@ -18,26 +25,37 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-    
-    def edit
-      @user = User.find(params[:id])
+  
+  def edit
+  end
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "プロフィールが更新されました。"
+      redirect_to @user
+    else
+      render 'edit'
     end
-    
-    def update
-      @user = User.find(params[:id])
+  end
+  
+  def following_users
+    @user = User.find(params[:id])
+    @users = @user.following_users #.paginate(page: params[:page])
+    # render 'show_follow'
+  end
+  
+  def follower_users
+    @user  = User.find(params[:id])
+    @users = @user.follower_users #.paginate(page: params[:page])
+    # render 'show_follow'
+  end
 
-      if @user.update(user_params)
-        redirect_to current_user , notice: 'ユーザー情報を編集しました'
-      else
-        render 'edit'
-      end
-    end
-    
-
-private
+  private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :nickname, :gender, :birthday, :description)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  
+    
   
 end
