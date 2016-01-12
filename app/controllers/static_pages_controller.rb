f5223f42b6a21ebe89b68178c2d47956aa367748
travelistars <travelistars@gmail.com>
2016-01-12 08:14:44 +0000

    fix local git repository location

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+I18n.locale = "ja"
+class StaticPagesController < ApplicationController
+  def home
+    if logged_in?
+    @micropost = current_user.microposts.build
+    @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+    end
+  end
+end
--- microposts/app/controllers/static_pages_controller.rb
+++
@@ -1,6 +0,0 @@
-I18n.locale = "ja"
-class StaticPagesController < ApplicationController
-  def home
-    @micropost = current_user.microposts.build if logged_in?
-  end
-end
