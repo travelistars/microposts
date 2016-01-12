82a2e14b7d7c91b44fa6e33b7d45fece50db3efd
travelistars <travelistars@gmail.com>
2016-01-12 13:20:13 +0000

    added security function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 I18n.locale = "ja"
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+  before_action :logged_in_user, only:[:index, :edit, :update, :destroy,
                                         :following_users, :follower_users]
   before_action :correct_user, only:[:edit, :update]
   def index
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @microposts = @user.microposts.order(created_at: :desc)
+    #@microposts = @user.microposts.order(created_at: :desc)
   end
   
   def new
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample app!"
-      redirect_to @user
+      redirect_to login_url
     else
       render 'new'
     end
@@ -53,11 +53,12 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :nickname, :gender, :birthday, :description)
   end
   
-  def logged_in_user
-      unless logged_in?
+    def logged_in_user
+        unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
@@ -66,7 +67,8 @@ class UsersController < ApplicationController
     # 正しいユーザーかどうか確認
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless @user == current_user
+      flash[:danger] = "Please log in."
+      redirect_to(login_url) unless @user == current_user
     end
   
     
