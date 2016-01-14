edab2ddea42e47d7e763da26646eb4e4a7e1e56f
travelistars <travelistars@gmail.com>
2016-01-14 08:16:43 +0000

    saved

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,7 +5,7 @@ class MicropostsController < ApplicationController
     def create
         @micropost = current_user.microposts.build(micropost_params)
         if @micropost.save
-            flash[:success] = "Micropost created!"
+            flash[:success] = "Micropostが作成されました。"
             redirect_to root_url
         else
             @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
@@ -17,7 +17,7 @@ class MicropostsController < ApplicationController
         @micropost = current_user.microposts.find_by(id: params[:id])
         return redirect_to root_url if @micropost.nil?
         @micropost.destroy
-        flash[:success] = "Micropost deleted"
+        flash[:success] = "Micropostを削除しました。"
         redirect_to request.referrer || root_url
     end 
     
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,7 +64,7 @@ class UsersController < ApplicationController
     def logged_in_user
         unless logged_in?
         store_location
-        flash[:danger] = "Please log in."
+        flash[:danger] = "ログインしてください。"
         redirect_to login_url
       end
     end
@@ -73,7 +73,7 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       unless @user == current_user
-        flash[:danger] = "Please log in."
+        flash[:danger] = "ログインしてください。"
         redirect_to login_url #unless @user == current_user
       end
     end
