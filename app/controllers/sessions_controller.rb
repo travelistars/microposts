a9843400df5dde0d5f7c1f2feef0e58f19019171
travelistars <travelistars@gmail.com>
2016-01-14 08:13:50 +0000

    updated ja.yml

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
       flash[:info] = "logged in as #{@user.name}"
       redirect_to @user
     else
-      flash[:danger] = 'invalid email/password combination'
+      flash[:danger] = 'メールアドレスとパスワードの組み合わせが間違っています。'
       render 'new'
     end
   end
