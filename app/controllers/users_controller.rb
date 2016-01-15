2f903f4e9d76f05604f6caf2cf7997c104b21310
travelistars <travelistars@gmail.com>
2016-01-15 09:50:08 +0000

    amend gender

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,7 +54,7 @@ class UsersController < ApplicationController
 private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :nickname, :birthday, :description)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :nickname, :birthday, :description, :gender)
   end
   
   def logged_in_user
